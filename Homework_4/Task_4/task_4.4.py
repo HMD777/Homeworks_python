884784d8fd0ade4c34a3fc915a11befcad481b27
Abdukhamid <a.davurov@gmail.com>
2022-11-07 19:08:36 +0600

    Completed Task #4 Homework #4

---
+++ Homework_4/Task_4/task_4.4.py
@@ -0,0 +1,38 @@
+import random
+
+print('Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.')
+
+
+
+def solver (k):
+    result = ""
+    while k > 0:
+        if k > 1:
+            z = random.randint(0,100)
+            if z == 0:
+                result = result + 'X**' + str(k) + ' + '
+            else:
+                result = result + str(z)+ '*X**' + str(k) + ' + '
+            k-=1
+        elif k == 1:
+            z = random.randint(0,100)
+            y = random.randint(0,100)
+            if z == 0:
+                result = result + 'X' + ' + ' + str (y)
+            else:
+                result = result + str(z)+ '*X' + ' + ' + str (y)
+            k-=1
+    return result
+
+def copy_to_file(file_name, res):
+    file = open(file_name, 'w')
+    file.write(res)
+    file.close()
+
+
+k = int(input('Введите степень k: '))
+result = solver (k)
+print (result)
+file_name = 'task_4.4.txt'
+copy_to_file(file_name, result)
+
